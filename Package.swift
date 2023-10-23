af0c927d86087970981170b480327935f7042a96
Theo Vora <theo.vora@gmail.com>
2023-10-22 23:30:17 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SexagenaryCycle1924",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SexagenaryCycle1924",
+            targets: ["SexagenaryCycle1924"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SexagenaryCycle1924"),
+        .testTarget(
+            name: "SexagenaryCycle1924Tests",
+            dependencies: ["SexagenaryCycle1924"]),
+    ]
+)
