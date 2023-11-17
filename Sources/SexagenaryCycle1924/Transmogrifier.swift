13a6c1d27b4adf36fbb1317d0bbd9b25a1de35a7
Theo Vora <theo.vora@gmail.com>
2023-11-17 12:00:11 -0600

    refactor: abstract the work of date extraction

--- Sources/SexagenaryCycle1924/Transmogrifier.swift
+++ Sources/SexagenaryCycle1924/Transmogrifier.swift
@@ -19,16 +19,8 @@ public struct Transmogrifier {
     private mutating func mapAnimals() {
         guard let wikipedia = wikipedia else { return }
         animals = wikipedia.map({ line in
-            let dates = line.yearBefore1983.split(separator: "–")
             
-            let dateStr1 = String(dates[0])
-            let dateStr2 = String(dates[1])
-            
-            let formatter = chineseDateFormatter(dateFormat: "MMM dd yyyy")
-            
-            guard let date1 = formatter.date(from: dateStr1),
-                  let date2 = formatter.date(from: dateStr2)
-            else { fatalError() }
+            let (date1, date2) = line.datesBefore1983
             
             return SexagenaryAnimal(
                 animal: line.animal,
@@ -48,5 +40,22 @@ public struct Transmogrifier {
         formatter.dateFormat = dateFormat
         
         return formatter
+    }
+}
+
+extension WikipediaLine {
+    fileprivate var datesBefore1983: (Date, Date) {
+        let dates = yearBefore1983.split(separator: "–")
+        
+        let dateStr1 = String(dates[0])
+        let dateStr2 = String(dates[1])
+        
+        let formatter = Transmogrifier([]).chineseDateFormatter(dateFormat: "MMM dd yyyy")
+        
+        guard let date1 = formatter.date(from: dateStr1),
+              let date2 = formatter.date(from: dateStr2)
+        else { fatalError() }
+        
+        return (date1, date2)
     }
 }
