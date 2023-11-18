e1f32dab380df5894e565bbec597b76da2011d5f
Theo Vora <theo.vora@gmail.com>
2023-11-18 16:06:44 -0600

    refactor: utilize notEmpty

--- Tests/SexagenaryCycle1924Tests/JSONFileReaderTests.swift
+++ Tests/SexagenaryCycle1924Tests/JSONFileReaderTests.swift
@@ -5,13 +5,13 @@ final class JSONFileReaderTests: XCTestCase {
     func test_load_givesModelRecords() {
         let sut = JSONFileReader()
         let records: [WikipediaLine] = sut.load()
-        XCTAssertFalse(records.isEmpty)
+        XCTAssertTrue(records.notEmpty)
     }
     
     func test_load_yieldsRecordWithAnAnimal() {
         let sut = JSONFileReader()
         let records: [WikipediaLine] = sut.load()
-        XCTAssertFalse(records.first?.animal.isEmpty ?? true)
+        XCTAssertTrue(records.first?.animal.notEmpty ?? false)
     }
     
     func test_decode_yieldsRecordWithAnAnimal() {
