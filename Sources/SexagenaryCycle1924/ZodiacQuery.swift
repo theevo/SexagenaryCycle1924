fd43a6d4942072d13bee512f1f21c4ac11cc42ff
Theo Vora <theo.vora@gmail.com>
2023-11-27 17:02:44 -0600

    refactor: move the work of reading the JSON file and synthesizing Swift objects to a new model
    this has the additional benefit of simplifying the public interface of the ZodiacQuery

--- Sources/SexagenaryCycle1924/ZodiacQuery.swift
+++ Sources/SexagenaryCycle1924/ZodiacQuery.swift
@@ -10,47 +10,10 @@ import Foundation
 public struct ZodiacQuery {
     public var animal: SexagenaryAnimal?
     
-    let animals: [SexagenaryAnimal]
+    let records: ZodiacRecords
     
     public init(birthday: String) throws {
-        let records: [WikipediaLine] = JSONFileReader().load()
-        self.animals = try Transmogrifier(records).animals
-        
-        self.animal = try animalWith(birthday: birthday)
-    }
-    
-    fileprivate func animalWith(birthday: String) throws -> SexagenaryAnimal {
-        let formatter = DateFormatter.inUTCTimeZone(dateFormat: "MM-dd-yyyy")
-        guard let swiftDate = formatter.date(from: birthday) else {
-            throw Error.invalidBirthdayInputString(date: birthday)
-        }
-        
-        guard let animal = contains(date: swiftDate) else {
-            throw Error.noAnimalFoundWithThatBirthday(date: birthday)
-        }
-        
-        return animal
-    }
-    
-    fileprivate func contains(date: Date) -> SexagenaryAnimal? {
-        return animals.first { animal in
-            animal.contains(date: date)
-        }
-    }
-}
-
-extension ZodiacQuery {
-    enum Error: LocalizedError {
-        case invalidBirthdayInputString(date: String)
-        case noAnimalFoundWithThatBirthday(date: String)
-        
-        var description: String {
-            switch self {
-            case .invalidBirthdayInputString(let date):
-                return "Invalid birthdate: \(date). Format must be MM-DD-YYYY."
-            case .noAnimalFoundWithThatBirthday(let date):
-                return "Could not find animal matching this date: \(date). This API only covers the years 1924 - 2044."
-            }
-        }
+        self.records = try ZodiacRecords()
+        self.animal = try records.animalWith(birthday: birthday)
     }
 }
