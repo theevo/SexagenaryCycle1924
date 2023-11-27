2e7461679215d4d13a8573529ffb8eb8c8f825fb
Theo Vora <theo.vora@gmail.com>
2023-11-27 15:47:17 -0600

    refactor: rename for readability of dates

--- Tests/SexagenaryCycle1924Tests/ZodiacQueryTests.swift
+++ Tests/SexagenaryCycle1924Tests/ZodiacQueryTests.swift
@@ -10,25 +10,25 @@ import SexagenaryCycle1924
 
 final class ZodiacQueryTests: XCTestCase {
 
-    func test_whenBirthday11261978_animalIsHorse() {
+    func test_whenBirthday11_26_1978_animalIsHorse() {
         let query = ZodiacQuery()
         let animal = query.birthday(date: "11-26-1978")
         XCTAssertEqual(animal.name, "Horse")
     }
     
-    func test_whenBirthday06011980_animalIsMonkey() {
+    func test_whenBirthday06_01_1980_animalIsMonkey() {
         let query = ZodiacQuery()
         let animal = query.birthday(date: "06-01-1980")
         XCTAssertEqual(animal.name, "Monkey")
     }
     
-    func test_whenBirthday03012017_animalIsRooster() {
+    func test_whenBirthday03_01_2017_animalIsRooster() {
         let query = ZodiacQuery()
         let animal = query.birthday(date: "03-01-2017")
         XCTAssertEqual(animal.name, "Rooster")
     }
     
-    func test_whenBirthday09011985_animalIsRat() {
+    func test_whenBirthday09_01_1985_animalIsRat() {
         let query = ZodiacQuery()
         let animal = query.birthday(date: "09-01-1985")
         XCTAssertEqual(animal.name, "Ox")
